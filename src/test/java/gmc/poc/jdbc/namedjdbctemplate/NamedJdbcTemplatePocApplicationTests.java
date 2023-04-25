fca0c03218932684572281f538ef920810d73f10
Navin3d <smnavin65@mail.com>
2023-04-25 12:35:16 +0530

    Initial Commit

---
+++ src/test/java/gmc/poc/jdbc/namedjdbctemplate/NamedJdbcTemplatePocApplicationTests.java
@@ -0,0 +1,13 @@
+package gmc.poc.jdbc.namedjdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamedJdbcTemplatePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
